caeae90277e366512f827b1aee431501ca8d0eff
Dhruv Jain <dhruv23198@iiitd.ac.in>
2026-07-03 03:46:59 +0530

    changes

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
-# import streamlit as st
-# def main():
-#     st.title("Hello, Streamlit!")
-#     st.write("This is a simple Streamlit app.")
-#     name=st.text_input("Enter your name:")
-#     col1,col2=st.columns(2)
-#     with col1:
-#         if(st.button("display by name",type="primary",key="btn1",width="stretch")):
-#             st.write(f"Hello, {name}!")
-#     with col2:
-#         if(st.button("display by name",type="secondary",key="btn2",width="stretch")):
-#             st.write(f"Welcome, {name}!")
-#     st.markdown("# This is a markdown text.")
-#     st.markdown("""
-#                 <style>
-#                     button{
-#                         background:green !important;
-#                     }
-#                 </style>"""
-#                 ,unsafe_allow_html=True)
-# main()
+
 import streamlit as st
+
 from src.screens.home_screen import home_screen
 from src.screens.teacher_screen import teacher_screen
 from src.screens.student_screen import student_screen
+
+from src.components.dialog_auto_enroll import auto_enroll_dialog
+
 def main():
-    if "login_type" not in st.session_state:
-        st.session_state["login_type"]=None
-    match st.session_state["login_type"]:
-        case "teacher":
+    st.set_page_config(
+        page_title='AttendAI - Making Attendance faster using AI',
+        page_icon= "https://i.ibb.co/YTYGn5qV/logo.png"
+    )
+    if 'login_type' not in st.session_state:
+        st.session_state['login_type'] = None
+
+    match st.session_state['login_type']:
+        case 'teacher':
             teacher_screen()
-        case "student":
+
+        case 'student':
             student_screen()
+        
         case None:
             home_screen()
+
+
+    join_code = st.query_params.get('join-code')
+    if join_code:
+        if st.session_state.login_type != 'student':
+            st.session_state.login_type = 'student'
+            st.rerun()
+        if st.session_state.get('is_logged_in') and st.session_state.get('user_role') == 'student':
+            auto_enroll_dialog(join_code)
 main()
